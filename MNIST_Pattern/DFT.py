42d348629ef4756e5796141f1a1c9f6b0ae62c34
HaohanWang <haohanw@andrew.cmu.edu>
2018-09-10 10:55:43 -0400

    code update

--- MNIST_Pattern/DFT.py
+++ MNIST_Pattern/DFT.py
@@ -290,6 +290,7 @@ def addMultiDomainPattern(r, l, testCase, testingFlag=False, randomMask=None, ra
             return addingPattern(r, radioMask)
     else:
         if l < 5:
+        # if np.random.random() < 0.5:
             k = 1
         else:
             k = 2
